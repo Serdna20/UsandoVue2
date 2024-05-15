a81fb59d7655e724b71ad945bb9ae9d5c79ca275
Serdna20 <afgarciasa@ufpso.edu.co>
2024-05-15 12:43:42 -0500

    Clase 15-05 Vue

---
+++ src/components/Vue/Directives.vue
@@ -0,0 +1,37 @@
+<script setup>
+    import { ref } from 'vue'
+    let msg = ref('Soy un tooltip')
+    let checked = ref(false)
+    let data = ["hola", "mundo", "bonito"]
+
+
+
+    let filtered = data.filter((word) => word.includes('o'))
+
+    // COMENTARIO PICKED INDEX
+    // let pickedIndex = ref(0)
+</script>
+
+<!-- This isn't a div -->
+<!-- Use : para llamar a una variable del script setup -->
+<template>
+    <!-- COMENTARIO #PICKED INDEX -->
+    <!-- <input type="number" v-model="pickedIndex"> -->
+    <input 
+    id="hola" 
+    type="text" 
+    class="border"
+    :class="{'border-red-500':checked}"
+    v-model="msg">
+    <div :title="msg">
+        {{ msg }}
+    </div>
+    <label for="check">Seleccionado {{checked}}</label>
+    <input type="checkbox" v-model="checked">
+    <ul>
+        <li v-for="(text, index) in data" :key="index" >{{index}}{{ text }}</li>
+        <li v-for="(text, index) in filtered" :key="index">{{ text }}</li>
+        <!-- COMENTARIO #PICKED INDEX -->
+        <!-- <h1>{{ data[pickedIndex] }}</h1> -->
+    </ul>
+</template>
---
+++ src/components/Vue/TODO.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+    import { ref } from 'vue';
+    let tasks = ref([])
+    let inputTask = ref('')
+
+    function addTask() {
+        if (inputTask.value.length != 0) {
+            tasks.value.push(inputTask.value)
+        }
+        inputTask.value = ''
+    }
+
+</script>
+
+<template>
+    <section class="py-8 px-10 max-w-96 bg-slate-300 rounded shadow-sm mx-auto mt-10">
+        <h1 class="text-center text-lg">Lista de tareas</h1>
+        <div class="mt-4 mb-2">
+            <input v-model="inputTask" class="px-2 w-4/5 outline-none">
+            <button @click="addTask" class="w-1/5 bg-green-600 text-white font-bold text-center">+</button>
+        </div>
+
+        <div v-for="(homework, index) in tasks" class="bg-white/60 px-2">Tarea #{{ index+1 }} - {{homework}}</div>
+    </section>
+</template>
